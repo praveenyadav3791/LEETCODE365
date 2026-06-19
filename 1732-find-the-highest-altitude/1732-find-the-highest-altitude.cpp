ca7ab16e75cb4491e4a0c66e8fa183decade922b
Praveen Yadav <160412897+praveenyadav3791@users.noreply.github.com>
2026-06-20 03:30:10 +0530

    Time: 0 ms (100%), Space: 10.8 MB (44.61%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+      int  current = 0;
+      int maxAlt =  0;
+      for(int g : gain){
+        current += g;
+        maxAlt = max(maxAlt, current); 
+      }  
+      return maxAlt;
+    }
+};
